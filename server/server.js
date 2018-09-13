fe710c717810ce3ed74a07e3f812ec069053243c
Juan <jmejio@gmail.com>
2018-09-12 23:34:40 -0400

    add Delete todos by id route

--- server/server.js
+++ server/server.js
@@ -54,6 +54,27 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+
+// --- Start DELETE
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 // --- listen 
 app.listen(port, () => {
     console.log(`Started at port ${port}`);
